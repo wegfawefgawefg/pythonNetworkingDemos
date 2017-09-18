d0abf497da51b63bb8ff3f9616936a7334c2d576
wegfawefgawefg <668es218pur@gmail.com>
2017-09-18 04:07:53 -0500

    formatting consistencies
    client.py was not formatted like the server.py
    now it is

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 import socket
 
-#   setup
+#   -----   setup   -----   #
 socketToServer = socket.socket()
-serverIP = "76.30.234.227"
+#T430SIP = 76.30.234.227
+jacobsLaptopIP = 98.198.242.96
+serverIP = jacobsLaptopIP
 port = 1337
 
-#   be a client
+#   -----   be a client -----   #
 socketToServer.connect( (serverIP, port) )
 bytesFromServer = socketToServer.recv( 1024 )
 messageFromServer = bytesFromServer.decode()
