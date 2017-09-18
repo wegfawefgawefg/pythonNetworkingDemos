fc871cd7d0d5dcf9fb7c71c8c4f5ac96fd9be2e6
wegfawefgawefg <668es218pur@gmail.com>
2017-09-18 07:18:34 -0500

    fullServer complete overhaul
    almost a full rewrite...

--- fullServer.py
+++ fullServer.py
@@ -1,47 +1,68 @@
 import socket
+import collections
 
-#   setup
+#   -----   setup   -----   #
 serverSocket = socket.socket()
 #serverSocket.setblocking(0)
-serverSocket.settimeout(0.1)
-host = "10.0.0.43"  # socket.gethostname( )
-print( host )
+hostLocalIP = "10.0.0.43"
+print( hostLocalIP )
 port = 1337
-serverSocket.bind( (host, port) )
+serverSocket.bind( (hostLocalIP, port) )
 
 greeting = "Welcome, traveler."
 
-#   be a server
+#   -----   setup state    -----   #
+#   list of all connected clients
+connectedClients = []
+
+#   newMesssages container
+newMessages = collections.deque()
+
+######!!!!! need to come up with a way to test for timeouts from all the connected clients periodically
+
+#   -----   be a server -----   #
 serverSocket.listen(5)
-connections = []
 
 while True:
-    #   look for a new connection
-    connection = None
-    try:
-        connection, address = serverSocket.accept()
-    except socket.timeout:
-        pass
-    if connection is not None:
-        print( "New Traveler: ", address )
-        connection.send( greeting.encode() )
-        #   add connection to connections list
-        connections.append( connection )
-        connection = None
+    checkForNewMessagesFromClients( connectedClients )
+    checkForNewClientConnection( connectedClients )
+    #send all new messages if there are any @@@@THIS NEEEDS TO BE ADDED@@@@@#
 
-
-    #   check through connection list for new messageFromServer
-    for transmitter in connections:
-        rawMessage = None
+#   check for new messages from clients
+def checkForNewMessagesFromClients( connectedClients ):
+    for connectedClient in connectedClients:
+        connectedClient.settimeout( 0.01 )
+        bytesFromClient = None
         try:
-            rawMessage = transmitter.recv( 1024 )
+            bytesFromClient = connectedClient.recv( 1024 )
         except socket.timeout:
             pass
-        if rawMessage is not None:
-            print( "Traveler ", transmitter.getsockname(), "says: ", rawMessage.decode() )
-            #   trasmit that message to all other connections in the list
-            for receiver in connections:
-                #   do not send the message to the original sender
-                if receiver == transmitter:
-                    break
-                receiver.send( rawMessage )
+        if rawMessageFromClient is not None:
+            messageFromClient = bytesFromClient.decode()
+            dealWithNewMessageFromClient( messageFromClient )
+
+#   deal with new message from a connected clients
+def dealWithNewMessageFromClient( messageFromClient ):
+    #   this is where we would check if the message is a ping
+    #!!!!!not implemented yet though!!!!!#
+    #   if message is not a response to a client request
+    #   #   then add it to the deque of new messages
+    newMessages.append( messageFromClient )
+
+#   check for new client connection
+def checkFoNewClientConnection( connectedClients ):
+        newClientConnection = None
+        try:
+            newClientConnection, address = serverSocket.accept()
+        except socket.timeout:
+            pass
+        if newClientConnection is not None:
+            dealWithNewClientConnection( newClientConnection, newClientIPAddress )
+
+#   deal with new client connection
+def dealWithNewClientConnection( newClientConnection, newClientIPAddress ):
+    print( "New Traveler: ", newClientIPAddress )
+    connection.send( greeting.encode() )
+
+    #   add connection to connections list
+    connections.append( connection )
